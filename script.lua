5a9c59f310443a191c9c553ee5ec3603c3d01852
Gabriel Blank Stift Mousquer <gabriel.mousquer@digitaldesk.com.br>
2018-09-11 17:41:56 -0300

    testing crossovers

--- script.lua
+++ script.lua
@@ -1,14 +1,12 @@
 local math = require 'math'
 local os = require 'os'
-local mutation_probability = 1 / 100
+local mutation_probability = 5
 
 local function generate_fitness_function(multipliers, constraints)
-    local multipliers_count = #multipliers
-
     local function multiply(solution, multipliers)
         local total = 0
 
-        for i = 1, multipliers_count do
+        for i = 1, #solution do
             total = total + solution[i] * multipliers[i]
         end
 
@@ -16,6 +14,12 @@ local function generate_fitness_function(multipliers, constraints)
     end
 
     return function(solution)
+        for _, v in ipairs(solution) do
+            if v <= 0 then
+                return -1
+            end
+        end
+
         for _, constraint in ipairs(constraints) do
             if multiply(solution, constraint.lhs) > constraint.rhs then
                 return 0
@@ -29,22 +33,19 @@ end
 local function crossover(solution1, solution2)
     local max = #solution1
     local new_solution = {}
-    local variation
 
     for i = 1, max do
-        if math.random() > mutation_probability then
+        if math.random(100) > mutation_probability then
             if i % 2 == 0 then
                 new_solution[i] = solution1[i]
             else
                 new_solution[i] = solution2[i]
             end
         else
-            variation = math.abs(solution1[i] - solution2[i] + 1) * 2
-
             if math.random() > 0.5 then
-                new_solution[i] = solution1[i] + variation
+                new_solution[i] = solution1[i] + 1
             else
-                new_solution[i] = solution1[i] - variation
+                new_solution[i] = solution1[i] - 1
             end
         end
     end
@@ -59,35 +60,45 @@ local function initialize_random_module()
     end
 end
 
+local function update_next_generation(fitness, population)
+    table.sort(population, function (solution1, solution2)
+        return fitness(solution1) > fitness(solution2)
+    end)
+
+    local k = #population / 10
+
+    for i = k, #population do
+        solution1 = population[math.random(k)]
+        solution2 = population[math.random(k)]
+        population[i] = crossover(solution1, solution2)
+    end
+end
+
 function test()
     initialize_random_module()
 
-    local fitness = generate_fitness_function({9, 14}, {
-        {lhs = {60, 80}, rhs = 3200},
-        {lhs = {1, 2}, rhs = 120},
-        {lhs = {4, 5}, rhs = 900},
+    local fitness = generate_fitness_function({2, 3, 4}, {
+        {lhs = {2, 3, 1}, rhs = 10},
+        {lhs = {2, 3, 3}, rhs = 15},
+        {lhs = {1, 1, 1}, rhs = 8},
     })
-
-    local function comparer(solution1, solution2)
-        return fitness(solution1) > fitness(solution2)
-    end
 
     local population = {}
 
     for i = 1, 1000 do
-        population[i] = {math.random(1, 100), math.random(1, 100)}
+        population[i] = {
+            math.random(1, 10),
+            math.random(1, 10),
+            math.random(1, 20)
+        }
     end
 
-    table.sort(population, comparer)
-    local c = 0
-    for i = 1, #population do
-        local f = fitness(population[i])
-        if f > 0 then
-            c = c + 1
-            print(table.concat(population[i], ", "), f)
-        end
+    for i = 1, 1000 do
+        update_next_generation(fitness, population)
+        print(table.concat(population[1], ", "), fitness(population[1]))
+        -- print(fitness(population[1]))
     end
-    print(c)
+    print(table.concat(population[1], ", "), fitness(population[1]))
 end
 
 test()
